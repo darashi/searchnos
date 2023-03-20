f5cf86d1d09f3c9905984ee83ce4702acadbad9a
Yoji Shidara <dara@shidara.net>
2023-03-20 20:40:54 +0900

    Split indexes by date

--- src/indexer/main.rs
+++ src/indexer/main.rs
@@ -1,7 +1,8 @@
+use chrono::TimeZone;
 use elasticsearch::{
     http::transport::{SingleNodeConnectionPool, TransportBuilder},
-    indices::{IndicesCreateParts, IndicesExistsParts},
-    DeleteParts, Elasticsearch, GetParts, IndexParts,
+    indices::IndicesPutIndexTemplateParts,
+    DeleteParts, Elasticsearch, IndexParts, SearchParts,
 };
 use env_logger;
 use log::{error, info};
@@ -9,86 +10,83 @@ use nostr_sdk::prelude::*;
 use serde::Serialize;
 use std::env;
 
-async fn create_index(
+async fn create_index_template(
     es_client: &Elasticsearch,
-    index_name: &str,
+    template_name: &str,
 ) -> Result<(), Box<dyn std::error::Error>> {
-    if es_client
-        .indices()
-        .exists(IndicesExistsParts::Index(&[index_name]))
-        .send()
-        .await?
-        .status_code()
-        .is_success()
-    {
-        info!("index already exists; use it as-is");
-        return Ok(());
-    }
-
-    info!("index does not exist; create it");
+    info!("putting index template: {}", template_name);
     let res = es_client
         .indices()
-        .create(IndicesCreateParts::Index(&index_name))
+        .put_index_template(IndicesPutIndexTemplateParts::Name(template_name))
         .body(json!({
-            "settings": {
-                "index": {
-                    "number_of_shards": 1,
-                    "number_of_replicas": 0,
-                    "analysis": {
-                    "analyzer": {
-                        "ngram_analyzer": {
-                        "type": "custom",
-                        "tokenizer": "ngram_tokenizer",
-                        "filter": ["icu_normalizer", "lowercase"],
+            "index_patterns": ["nostr-*"],
+            "template": {
+                "settings": {
+                    "index": {
+                        "number_of_shards": 1,
+                        "number_of_replicas": 0,
+                        "analysis": {
+                        "analyzer": {
+                            "ngram_analyzer": {
+                            "type": "custom",
+                            "tokenizer": "ngram_tokenizer",
+                            "filter": ["icu_normalizer", "lowercase"],
+                            },
                         },
-                    },
-                    "tokenizer": {
-                        "ngram_tokenizer": {
-                        "type": "ngram",
-                        "min_gram": "1",
-                        "max_gram": "2",
+                        "tokenizer": {
+                            "ngram_tokenizer": {
+                            "type": "ngram",
+                            "min_gram": "1",
+                            "max_gram": "2",
+                            },
                         },
-                    },
+                        },
                     },
                 },
-            },
-            "mappings": {
-                "dynamic": false,
-                "properties": {
-                    "event": {
-                        "dynamic": false,
-                        "properties": {
-                            "content": {
-                                "type": "text",
-                                "index": false
-                            },
-                            "created_at": {
-                                "type": "date",
-                                "format": "epoch_second"
-                            },
-                            "kind": {
-                                "type": "integer"
-                            },
-                            "id": {
-                                "type": "keyword"
-                            },
-                            "pubkey": {
-                                "type": "keyword"
-                            },
-                            "sig": {
-                                "type": "keyword",
-                                "index": false
-                            },
-                            "tags": {
-                                "type": "keyword"
-                            },
+                "mappings": {
+                    "dynamic": false,
+                    "properties": {
+                        "event": {
+                            "dynamic": false,
+                            "properties": {
+                                "content": {
+                                    "type": "text",
+                                    "index": false
+                                },
+                                "created_at": {
+                                    "type": "date",
+                                    "format": "epoch_second"
+                                },
+                                "kind": {
+                                    "type": "integer"
+                                },
+                                "id": {
+                                    "type": "keyword"
+                                },
+                                "pubkey": {
+                                    "type": "keyword"
+                                },
+                                "sig": {
+                                    "type": "keyword",
+                                    "index": false
+                                },
+                                "tags": {
+                                    "type": "keyword"
+                                },
+                            }
+                        },
+                        "text": {
+                            "type": "text",
+                            "analyzer": "ngram_analyzer",
+                            "index": "true",
+                        },
+                        "language": {
+                            "type": "keyword"
                         }
-                    },
-                    "text": {
-                        "type": "text",
-                        "analyzer": "ngram_analyzer",
-                        "index": "true",
-                    },
+                    }
+                },
+                "aliases": {
+                    "nostr": {}
                 }
             }
         }))
@@ -109,19 +107,29 @@ struct Document {
     text: String,
 }
 
+fn index_name_for_event(prefix: &str, event: &Event) -> Result<String, Box<dyn std::error::Error>> {
+    let dt = chrono::Utc.timestamp_opt(event.created_at.as_i64(), 0);
+    if let Some(dt) = dt.single() {
+        Ok(format!("{}-{}", prefix, dt.format("%Y.%m.%d").to_string()))
+    } else {
+        Err(format!("failed to parse date: {}", event.created_at).into())
+    }
+}
+
 async fn handle_text_note(
     es_client: &Elasticsearch,
-    index_name: &str,
+    index_prefix: &str,
     event: &Event,
 ) -> Result<(), Box<dyn std::error::Error>> {
-    println!("{}", event.as_json());
+    let index_name = index_name_for_event(index_prefix, event)?;
+    println!("{} {}", index_name, event.as_json());
     let id = event.id.to_hex();
     let doc = Document {
         event: event.clone(),
         text: event.content.clone(),
     };
     let res = es_client
-        .index(IndexParts::IndexId(index_name, &id))
+        .index(IndexParts::IndexId(index_name.as_str(), &id))
         .body(doc)
         .send()
         .await?;
@@ -135,36 +143,61 @@ async fn handle_text_note(
 
 async fn delete_event(
     es_client: &Elasticsearch,
-    index_name: &str,
+    alias_name: &str,
     id: &str,
     pubkey: &XOnlyPublicKey,
 ) -> Result<(), Box<dyn std::error::Error>> {
     info!("try to delete: id={}", id);
+
     let res = es_client
-        .get(GetParts::IndexId(&index_name, &id))
+        .search(SearchParts::Index(&[alias_name]))
+        .body(json!({
+            "query": {
+                "term": {
+                    "_id": id
+                }
+            }
+        }))
+        .size(1)
         .send()
         .await?;
-    if res.status_code() == 404 {
-        return Err(format!("event not found; id={}", id).into());
-    }
+
     if !res.status_code().is_success() {
         let status_code = res.status_code();
         let body = res.text().await?;
         return Err(format!("failed to fetch; received {}, {}", status_code, body).into());
     }
-    let hit = res.json::<serde_json::Value>().await?;
+    let response_body = res.json::<serde_json::Value>().await?;
+    let hits_array = match response_body["hits"]["hits"].as_array() {
+        Some(hits) => hits,
+        None => return Err("Failed to retrieve hits from response".into()),
+    };
+    let hit = match hits_array.first() {
+        Some(hit) => hit,
+        None => return Err(format!("Event with ID {} not found in search results", id).into()),
+    };
 
-    let event = serde_json::from_value::<Event>(hit["_source"]["event"].clone())?;
+    let event_to_be_deleted = serde_json::from_value::<Event>(hit["_source"]["event"].clone())?;
 
-    let ok_to_delete = *pubkey == event.pubkey;
-    info!("can event {} be deleted? {}", event.id, ok_to_delete);
+    let ok_to_delete = *pubkey == event_to_be_deleted.pubkey;
+    info!(
+        "can event {} be deleted? {}",
+        event_to_be_deleted.id, ok_to_delete
+    );
     if !ok_to_delete {
         return Err(format!(
             "pubkey mismatch: pub key was {}, but that of the event {} was {}",
-            pubkey, id, event.pubkey
+            pubkey, id, event_to_be_deleted.pubkey
         )
         .into());
     }
+
+    let index_name = hit["_index"].as_str();
+    let index_name = match index_name {
+        Some(index_name) => index_name,
+        None => return Err("failed to get index name".into()),
+    };
+
     let res = es_client
         .delete(DeleteParts::IndexId(&index_name, &id))
         .send()
@@ -215,10 +248,11 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let conn_pool = SingleNodeConnectionPool::new(es_url);
     let es_transport = TransportBuilder::new(conn_pool).disable_proxy().build()?;
     let es_client = Elasticsearch::new(es_transport);
-    let index_name = "nostr";
-    create_index(&es_client, index_name).await?;
+    let index_template_name = "nostr";
+    create_index_template(&es_client, index_template_name).await?;
     info!("elasticsearch index ready");
 
+    // prepare nostr client
     let my_keys: Keys = Keys::generate();
     let nostr_client = Client::new(&my_keys);
 
@@ -241,10 +275,10 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
             if let RelayPoolNotification::Event(_url, event) = notification {
                 match event.kind {
                     Kind::TextNote => {
-                        handle_text_note(&es_client, &index_name, &event).await?;
+                        handle_text_note(&es_client, &index_template_name, &event).await?;
                     }
                     Kind::EventDeletion => {
-                        handle_deletion_event(&es_client, &index_name, &event).await?;
+                        handle_deletion_event(&es_client, &index_template_name, &event).await?;
                     }
                     _ => {
                         continue;
