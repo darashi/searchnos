f7884dd5b288de97f1246a8f21501ccaf9564b0c
Yoji Shidara <dara@shidara.net>
2024-08-04 18:11:13 +0900

    Remove useless &

--- build.rs
+++ build.rs
@@ -1,7 +1,7 @@
 use std::process::Command;
 fn main() {
     let output = Command::new("git")
-        .args(&["rev-parse", "HEAD"])
+        .args(["rev-parse", "HEAD"])
         .output()
         .expect("failed to execute git");
     let git_hash = String::from_utf8(output.stdout).expect("failed to obtain output");
